82596dde66a288ed1ccf7b6caa47e5a74e52f885
Michael D. Misamore <m.misamore@gmail.com>
2015-11-04 21:15:59 -0500

    Before switching to JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
-    }]
+      loader: 'react-hot!babel'
+    }],
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -18,6 +22,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
