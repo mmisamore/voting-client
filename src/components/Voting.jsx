69eb73cb4a96ba0e4f293e3d0980d165e10bd899
Michael D. Misamore <m.misamore@gmail.com>
2015-12-06 09:33:32 -0500

    Add some client-side tests for pure rendering

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
   render: function() {
-    return <div>
+     mixins: [PureRenderMixin],
+     return <div>
       {this.props.winner ?
         <Winner ref="winner" winner={this.props.winner} /> :
         <Vote {...this.props} />}
