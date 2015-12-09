cd393d88fa50d4e69642da367fd050e1eef55cef
Michael D. Misamore <m.misamore@gmail.com>
2015-12-09 17:57:01 -0500

    Add route handling for client, and rename Apps to App so it actually launches

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,12 +4,12 @@ import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
-     mixins: [PureRenderMixin],
      return <div>
       {this.props.winner ?
         <Winner ref="winner" winner={this.props.winner} /> :
         <Vote {...this.props} />}
-    </div>;
+     </div>
   }
 });
